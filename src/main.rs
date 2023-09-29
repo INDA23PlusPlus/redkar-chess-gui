fe11048e0335e56bb3a5d12efbe610417d522028
Raunak Redkar <raunak.redkar@gmail.com>
2023-09-29 11:12:03 +0200

    improving performance by not doing stupid thing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use ggez::event::MouseButton;
 use ggez::{Context, GameResult};
-use ggez::graphics::{self, Color, Rect};
+use ggez::graphics::{self, Color, Rect, Image};
 use ggez::GameError;
 use ggez::glam::*;
 use std::{env, path};
@@ -9,6 +9,18 @@ use chess_lib;
 const SQUARE: f32 = 125.0;
 
 struct State {
+    w_pawn: graphics::Image,
+    b_pawn: graphics::Image,
+    w_knight: graphics::Image,
+    b_knight: graphics::Image,
+    w_bishop: graphics::Image,
+    b_bishop: graphics::Image,
+    w_rook: graphics::Image,
+    b_rook: graphics::Image,
+    w_queen: graphics::Image,
+    b_queen: graphics::Image,
+    w_king: graphics::Image,
+    b_king: graphics::Image,
     game: chess_lib::ChessBoard,
     mouse_x: f32,
     mouse_y: f32,
@@ -71,40 +83,40 @@ impl ggez::event::EventHandler<GameError> for State {
                         Some(p) => {
                             match p  {
                                 chess_lib::ChessPiece::Pawn(chess_lib::Color::White) => {
-                                    canvas.draw(&(graphics::Image::from_path(ctx, "/w_pawn.png").unwrap()), print_pos);
+                                    canvas.draw(&self.w_pawn, print_pos);
                                 },
                                 chess_lib::ChessPiece::Pawn(chess_lib::Color::Black) => {
-                                    canvas.draw(&(graphics::Image::from_path(ctx, "/b_pawn.png").unwrap()), print_pos);
+                                    canvas.draw(&self.b_pawn, print_pos);
                                 },
                                 chess_lib::ChessPiece::Knight(chess_lib::Color::White) => {
-                                    canvas.draw(&(graphics::Image::from_path(ctx, "/w_knight.png").unwrap()), print_pos);    
+                                    canvas.draw(&self.w_knight, print_pos);
                                 },
                                 chess_lib::ChessPiece::Knight(chess_lib::Color::Black) => {
-                                    canvas.draw(&(graphics::Image::from_path(ctx, "/b_knight.png").unwrap()), print_pos);
+                                    canvas.draw(&self.b_knight, print_pos);
                                 },
                                 chess_lib::ChessPiece::Bishop(chess_lib::Color::White) => {
-                                    canvas.draw(&(graphics::Image::from_path(ctx, "/w_bishop.png").unwrap()), print_pos);
+                                    canvas.draw(&self.w_bishop, print_pos);
                                 },
                                 chess_lib::ChessPiece::Bishop(chess_lib::Color::Black) => {
-                                    canvas.draw(&(graphics::Image::from_path(ctx, "/b_bishop.png").unwrap()), print_pos); 
+                                    canvas.draw(&self.b_bishop, print_pos);
                                 },
                                 chess_lib::ChessPiece::Rook(chess_lib::Color::White) => {
-                                    canvas.draw(&(graphics::Image::from_path(ctx, "/w_rook.png").unwrap()), print_pos); 
+                                    canvas.draw(&self.w_rook, print_pos);
                                 },
                                 chess_lib::ChessPiece::Rook(chess_lib::Color::Black) => {
-                                    canvas.draw(&(graphics::Image::from_path(ctx, "/b_rook.png").unwrap()), print_pos); 
+                                    canvas.draw(&self.b_rook, print_pos);
                                 },
                                 chess_lib::ChessPiece::Queen(chess_lib::Color::White) => {
-                                    canvas.draw(&(graphics::Image::from_path(ctx, "/w_queen.png").unwrap()), print_pos); 
+                                    canvas.draw(&self.w_queen, print_pos);
                                 },
                                 chess_lib::ChessPiece::Queen(chess_lib::Color::Black) => {
-                                    canvas.draw(&(graphics::Image::from_path(ctx, "/b_queen.png").unwrap()), print_pos);    
+                                    canvas.draw(&self.b_queen, print_pos);
                                 },
                                 chess_lib::ChessPiece::King(chess_lib::Color::White) => {
-                                    canvas.draw(&(graphics::Image::from_path(ctx, "/w_king.png").unwrap()), print_pos);  
+                                    canvas.draw(&self.w_king, print_pos);
                                 },
                                 chess_lib::ChessPiece::King(chess_lib::Color::Black) => {
-                                    canvas.draw(&(graphics::Image::from_path(ctx, "/b_king.png").unwrap()), print_pos);
+                                    canvas.draw(&self.b_king, print_pos);
                                 },
                             }
                         }, 
@@ -180,6 +192,12 @@ fn main() {
     } else {
         path::PathBuf::from("./resources")
     };
+
+    let (mut ctx, event_loop) = ggez::ContextBuilder::new("Chess", "Raunak Redkar")
+    .add_resource_path(resource_dir)
+    .window_mode(ggez::conf::WindowMode::default().dimensions(SQUARE * 8.0, SQUARE * 8.0))
+    .build()
+    .unwrap();
     let state = State {
         game: chess_lib::ChessBoard::create(), 
         mouse_x: 0.0, mouse_y: 0.0, 
@@ -188,12 +206,18 @@ fn main() {
         piece_selected: false,
         square_selected: false,
         possible_moves: Vec::<(usize, usize)>::new(),
+        w_pawn: graphics::Image::from_path(&ctx, "/w_pawn.png").unwrap(),
+        b_pawn: graphics::Image::from_path(&ctx, "/b_pawn.png").unwrap(),
+        w_knight: graphics::Image::from_path(&ctx, "/w_knight.png").unwrap(),
+        b_knight: graphics::Image::from_path(&ctx, "/b_knight.png").unwrap(),
+        w_bishop: graphics::Image::from_path(&ctx, "/w_bishop.png").unwrap(),
+        b_bishop: graphics::Image::from_path(&ctx, "/b_bishop.png").unwrap(),
+        w_rook: graphics::Image::from_path(&ctx, "/w_rook.png").unwrap(),
+        b_rook: graphics::Image::from_path(&ctx, "/b_rook.png").unwrap(),
+        w_queen: graphics::Image::from_path(&ctx, "/w_queen.png").unwrap(),
+        b_queen: graphics::Image::from_path(&ctx, "/b_queen.png").unwrap(),
+        w_king: graphics::Image::from_path(&ctx, "/w_king.png").unwrap(),
+        b_king: graphics::Image::from_path(&ctx, "/b_king.png").unwrap(),
     };
-
-    let (mut ctx, event_loop) = ggez::ContextBuilder::new("Chess", "Raunak Redkar")
-    .add_resource_path(resource_dir)
-    .window_mode(ggez::conf::WindowMode::default().dimensions(SQUARE * 8.0, SQUARE * 8.0))
-    .build()
-    .unwrap();
     ggez::event::run(ctx, event_loop, state);
 }
